c9d81830614455bb0f246d99370c7adaa7ba590c
YousefShata <yossef_shata_a@yahoo.com>
2023-11-02 19:00:58 -0700

    betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,6 +15,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
-	
+
 	return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
 }
