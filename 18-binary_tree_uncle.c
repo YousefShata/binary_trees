3a93159988f646326f24addc012767de3c7fd484
YousefShata <yossef_shata_a@yahoo.com>
2023-11-02 20:01:23 -0700

    ops

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * binary_tree_sibling - check the tree sibilings
+ * binary_tree_uncle - check the tree sibilings
  * @node: a node
  * Return: pointer to the sibiling node
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
