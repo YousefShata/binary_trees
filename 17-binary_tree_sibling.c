8b1a17d44c97150b248c628c9155de65f82bf412
YousefShata <yossef_shata_a@yahoo.com>
2023-11-02 19:54:18 -0700

    siblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * binary_tree_sibling - check the tree sibilings
+ * @node: a node
+ * Return: pointer to the sibiling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->right != NULL)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
