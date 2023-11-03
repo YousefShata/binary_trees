0cb1ef6fd4781e26b259c0c839c9a4682efee98e
YousefShata <yossef_shata_a@yahoo.com>
2023-11-02 18:37:13 -0700

    Balance enhancing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,14 +12,43 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	
+	if (tree->left == NULL)
+		left_len = 0;
+	else
+		left_len = 1 +((int) binary_tree_height(tree->left));
+	
+	if (tree->right == NULL)
+		right_len = 0;
+	else
+		right_len = 1 +((int) binary_tree_height(tree->right));
+	
+	diff = left_len - right_len;
+	return (diff);
+}
+
+/**
+ * binary_tree_height - check the tree height
+ * @tree: tree root
+ * Return: the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_len, left_len;
+
+	if (tree == NULL)
+		return (0);
 	if (tree->left)
-		left_len = 1 + binary_tree_balance(tree->left);
+		left_len = 1 + binary_tree_height(tree->left);
 	else
 		left_len = 0;
 	if (tree->right)
-		right_len = 1 + binary_tree_balance(tree->right);
+		right_len = 1 + binary_tree_height(tree->right);
 	else
 		right_len = 0;
-	diff = left_len - right_len;
-	return (diff);
+
+	if (left_len > right_len)
+		return (left_len);
+	else
+		return (right_len);
 }
